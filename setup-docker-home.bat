0f7a4f9f1f847ea005e3dd98de0a935c586ae95b
moo <34907770+moo-the-cow@users.noreply.github.com>
2026-06-09 11:41:49 +0200

    important security patches

--- setup-docker-home.bat
+++ setup-docker-home.bat
@@ -34,7 +34,7 @@ echo.
 echo Configuring .env for HOME version...
 
 :: Create .env with HOME configuration only (username/password, no encryption)
-echo GLOBAL_RIST_VERSION=0.0.22 >> .env
+echo GLOBAL_RIST_VERSION=0.0.23 >> .env
 echo. >> .env
 echo ##### Home network setup ####### >> .env
 echo. >> .env
